1c036eedf37c420bd0e23f6dc6151d7d16b17827
Ashley Villella <ashleyjagdeo@gmail.com>
2022-04-26 01:18:57 -0400

    endpoint created for add comment to the edit page

--- client/src/components/EditContent/EditContent.jsx
+++ client/src/components/EditContent/EditContent.jsx
@@ -8,25 +8,78 @@ const { v4: uuidv4 } = require("uuid");
 export default class EditContent extends React.Component {
   state = {
     editContent: null,
+    editedContent: null,
+    comments: null,
+    author: null,
   };
 
+  // GET THE CONTENT FROM GPT-3
   fetchData() {
     axios
       .get(`${API_URL}/posts/${this.props.match.params.id}`)
       .then((response) => {
-        this.setState({ editContent: response.data.content });
+        this.setState({
+          editContent: response.data.content,
+          comments: response.data.comments,
+        });
       });
   }
+
+  // TO POST A COMMENT TO THE DATABASE
+  onCommentClickHandler = async (e) => {
+    await axios
+      .post(`${API_URL}/posts/create/${this.props.match.params.id}/comments`, {
+        comments: this.state.comments,
+        author: this.state.author,
+      })
+      .then((response) => {
+        window.alert("submitted");
+      })
+      .catch((err) => console.log(err));
+  };
 
   componentDidMount() {
     this.fetchData();
   }
 
+  onEditEventHandler = async (e) => {
+    this.setState({
+      editedContent: e.response.data.content,
+    }).catch((err) => console.error(err));
+  };
+
   render() {
-    console.log(this.state.editContent);
     return (
       <>
-        <p>{this.state.editContent}</p>
+        <h2>Edit Content </h2>
+        <Box
+          component="form"
+          sx={{
+            "& > :not(style)": { m: 1, width: "100ch" },
+          }}
+        >
+          <TextField
+          // value={this.state.editContent}
+          // onChange={this.state.editedContent}
+          />
+        </Box>
+        <Box
+          component="form"
+          sx={{
+            "& > :not(style)": { m: 1, width: "100ch" },
+          }}
+        >
+          <TextField
+            label="Add a comment for the reviewer here"
+            variant="outlined"
+            onChange={this.state.comments}
+          />
+        </Box>
+        <Button onClick={this.onCommentClickHandler} variant="contained">
+          Send for Review
+        </Button>
+        <p>OR</p>
+        <Button variant="contained">Share on Social Media</Button>
       </>
     );
   }
--- server/routes/prompts.js
+++ server/routes/prompts.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const moment = require("moment");
 const router = express.Router();
 const fetchGPT3 = require("../lib/gpt3");
 const { v4: uuidv4 } = require("uuid");
@@ -32,7 +33,7 @@ router.post("/create", async (req, res) => {
   const content = req.body.content;
   console.log("test test test");
   const id = uuidv4();
-  const todaysDate = Date.now();
+  const todaysDate = moment().format("LL");
   const docRef = db.collection("posts").doc(id);
 
   await docRef.set({
@@ -40,6 +41,7 @@ router.post("/create", async (req, res) => {
     title: "on",
     createDate: todaysDate,
   });
+  console.log(docRef);
   res.status(200).send("ok");
 });
 
@@ -55,6 +57,22 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+// POST A COMMENT TO THE FIREBASE DATABASE AS ANOTHER COLLECTION
+
+router.post("/create/:id/comments", async (req, res) => {
+  const comment = req.body.comments;
+  const author = req.body.author;
+  const { id } = req.params;
+  const todaysDate = moment().format("LL");
+
+  const docRef = db.collection("posts").doc(id).collection("comments");
+
+  await docRef.add({
+    comments: comment,
+    author: author,
+  });
+  res.status(200).send("ok");
+});
 
 // FIREBASE DATABASE
 const serviceAccount = require("/Users/ashleyj/src/brainstation/assignments/CAPSTONE/copybot-xyz-firebase-adminsdk-dhhc2-09454f3b8e.json");
