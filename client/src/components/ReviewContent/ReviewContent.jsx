48a49c0fdd84dd1af3c09bcefd41613a82b19469
Ashley Villella <ashleyjagdeo@gmail.com>
2022-04-26 22:38:02 -0400

    created a new page for reviewer

--- client/src/components/ReviewContent/ReviewContent.jsx
+++ client/src/components/ReviewContent/ReviewContent.jsx
@@ -1,4 +1,4 @@
-import "./EditContent.scss";
+import "./ReviewContent.scss";
 import React from "react";
 import axios from "axios";
 import { API_URL } from "../../utils/utils";
@@ -7,51 +7,34 @@ const { v4: uuidv4 } = require("uuid");
 
 export default class EditContent extends React.Component {
   state = {
-    editContent: null,
+    title: null,
     editedContent: null,
+    createDate: null,
     comments: null,
     author: null,
   };
 
-  // GET THE CONTENT FROM GPT-3
-  fetchData() {
+  // GET THE COMMENTS FROM FIREBASE SERVER
+  getDataComments() {
     axios
-      .get(`${API_URL}/posts/${this.props.match.params.id}`)
+      .get(`{API_URL}/posts/create/${this.props.match.params.id}`)
       .then((response) => {
         this.setState({
-          editContent: response.data.content,
+          title: response.data.title,
+          editedContent: response.data.content,
           comments: response.data.comments,
+          createDate: response.data.createDate,
         });
       });
   }
 
-  // TO POST A COMMENT TO THE DATABASE
-  onCommentClickHandler = async (e) => {
-    await axios
-      .post(`${API_URL}/posts/create/${this.props.match.params.id}/comments`, {
-        comments: this.state.comments,
-        author: this.state.author,
-      })
-      .then((response) => {
-        window.alert("submitted");
-      })
-      .catch((err) => console.log(err));
-  };
+  // POST THE RESPONSE TO CONTENT (CHECKBOX, SIGNATURE & COMMENTS)
 
-  componentDidMount() {
-    this.fetchData();
-  }
-
-  onEditEventHandler = async (e) => {
-    this.setState({
-      editedContent: e.response.data.content,
-    }).catch((err) => console.error(err));
-  };
-
+  
   render() {
     return (
       <>
-        <h2>Edit Content </h2>
+        <h2>Review Content</h2>
         <Box
           component="form"
           sx={{
@@ -75,11 +58,10 @@ export default class EditContent extends React.Component {
             onChange={this.state.comments}
           />
         </Box>
-        <Button onClick={this.onCommentClickHandler} variant="contained">
-          Send for Review
+        <Checkbox checked={checked} onChange={handleChange} {...label} />
+        <Button onClick={this.onSomething} variant="contained">
+          Return to Drafter
         </Button>
-        <p>OR</p>
-        <Button variant="contained">Share on Social Media</Button>
       </>
     );
   }
